dc1565f1cec89ebd9013d040c8dd8d13701c8ffe
Ben Jordan <bej9038@rit.edu>
2021-03-15 10:36:43 -0400

    Changed Signin backend component to determine trial id based on largest trialid in the database rather than the number of users in the database.

--- Signin/Signin.php
+++ Signin/Signin.php
@@ -10,47 +10,48 @@ $subj = $_POST["name"];
 $currenttime = $_POST["currenttime"];
 $_SESSION["subj"] = $subj;
 
-////UserData
-//
-//$path = "/home2/auditot7/public_html/Implant Testing Program/UserData/" . $subj;
-//
-//if ($subj)
-//{
-//    if (!file_exists($path)) {
-//        mkdir($path, 0777, true);
-//    }
-//}
-//
-////DB
-//
-//$host = "localhost";
-//$user = "auditot7_bej9038";
-//$password = "jordan";
-//$db = "auditot7_UserData";
-//
-//$conn = new mysqli($host,$user,$password,$db);
-//if ($conn->connect_error) {
-//    die("Connection failed: " . $conn->connect_error);
-//}
-//
-////find trial id
-//$trialid = null;
-//$result = mysqli_query($conn, "SELECT TrialID FROM Users");
-//if($result)
-//{
-//    $trialid = mysqli_num_rows($result);
-//    $_SESSION["trialid"] = $trialid;
-//}
-//
-//$sql = "INSERT INTO Users (Username, TrialID, SignInTime)
-//VALUES ('$subj', $trialid, '$currenttime')";
-//
-//if ($conn->query($sql) !== TRUE) {
-//    echo "Error: " . $sql . "<br>" . $conn->error;
-//}
-//
-//mysqli_free_result($result);
-//$conn->close();
+//UserData
+
+$path = "/home2/auditot7/public_html/Implant Testing Program/UserData/" . $subj;
+
+if ($subj)
+{
+    if (!file_exists($path)) {
+        mkdir($path, 0777, true);
+    }
+}
+
+//DB
+
+$host = "localhost";
+$user = "auditot7_bej9038";
+$password = "jordan";
+$db = "auditot7_UserData";
+
+$conn = new mysqli($host,$user,$password,$db);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//find trial id
+$trialid = null;
+$result = mysqli_query($conn, "SELECT MAX(TrialID) as 'max' FROM Users");
+if($result)
+{
+    $row = mysqli_fetch_array($result);
+    $trialid = $row['max'] + 1;
+    $_SESSION["trialid"] = $trialid;
+}
+
+$sql = "INSERT INTO Users (Username, TrialID, SignInTime)
+VALUES ('$subj', $trialid, '$currenttime')";
+
+if ($conn->query($sql) !== TRUE) {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+mysqli_free_result($result);
+$conn->close();
 
 header('Location: /Implant Testing Program/Signin/SigninPage.html');
 exit;
