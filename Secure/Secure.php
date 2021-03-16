575c830c3a3533cd5b464ff793fc4181a3aeebc3
Ben Jordan <bej9038@rit.edu>
2021-03-16 12:53:01 -0400

    Working on admin mode

--- Secure/Secure.php
+++ Secure/Secure.php
@@ -5,7 +5,22 @@ $pword = $_POST['pword'];
 
 if($user == "admin" && $pword == "hcitraining")
 {
-    header('Location: /Implant Testing Program/Secure/Secure.html.php');
+
+    $host = "localhost";
+    $user = "auditot7_bej9038";
+    $password = "jordan";
+    $db = "auditot7_UserData";
+    $conn = new mysqli($host, $user, $password, $db);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $result = mysqli_query($conn, "SELECT Username, TrialID from Users");
+    $arr = mysqli_fetch_all($result);
+    echo json_encode($arr);
+    mysqli_free_result($result);
+
+    header('Location: /Implant Testing Program/Secure/Secure.html');
     exit;
 }
 else {
